fd7d754856a95aa68548454503fa1677d946b5e6
tsunamigg <zxllzjjbqysj@qq.com>
2020-06-26 01:26:56 +0800

    fix

--- week01/scrapy_maoyan/scrapy_maoyan/pipelines.py
+++ week01/scrapy_maoyan/scrapy_maoyan/pipelines.py
@@ -5,14 +5,14 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pandas
 
 class ScrapyMaoyanPipeline:
     def process_item(self, item, spider):
         name= item['name']
         tag = item['tag']
         time = item['time']
-        output = f'|{name}|\t|{tag}|\t|{time}|\n\n'
-        # print(output)
-        with open('maoyantop10.text','a+',encoding='utf-8') as article:
-            article.write(output)
-            return item
+        output = [f'|{name}|\t|{tag}|\t|{time}|\n\n']
+        
+        movie = pandas.DataFrame(data=output)
+        movie.to_csv('./maoyantop10.csv', mode='a', encoding='utf8',index=False, header=False)
--- week01/scrapy_maoyan/scrapy_maoyan/spiders/maoyan.py
+++ week01/scrapy_maoyan/scrapy_maoyan/spiders/maoyan.py
@@ -8,6 +8,7 @@ class MaoyanSpider(scrapy.Spider):
     name = 'maoyan'
     allowed_domains = ['maoyan.com']
     start_urls = ['https://maoyan.com/films?showType=3&offset=0']
+    
 
     def start_requests(self):
         headers = {
